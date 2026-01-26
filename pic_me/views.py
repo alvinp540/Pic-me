aa637017101d0a13dcc94b343312c46f16677c1b
alvinp540 <alvinparsaloi@gmail.com>
2026-01-26 14:50:03 +0300

    home view to display photo gallarey

--- pic_me/views.py
+++ pic_me/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    """
+    Display the photo gallery homepage with optional tag filtering.
+    """
+    photos = Photo.objects.all().prefetch_related('tags', 'interactions')
+    tags = Tag.objects.all()
+    
+    tag_filter = request.GET.get('tag')
+    if tag_filter:
+        photos = photos.filter(tags__slug=tag_filter)
+    
+    search_query = request.GET.get('search')
+    if search_query:
+        photos = photos.filter(
+            Q(title__icontains=search_query) | 
+            Q(description__icontains=search_query)
+        )
+    
+    context = {
+        'photos': photos,
+        'tags': tags,
+        'selected_tag': tag_filter,
+        'search_query': search_query,
+    }
+    return render(request, 'pic_me/home.html', context)
+
+
+def photo_detail(request, photo_id):
+    """
+    Display detailed information for a specific photo.
+    """
+    photo = get_object_or_404(Photo, id=photo_id)
+    user_interaction = None
+    
+    if request.user.is_authenticated:
+        user_interaction = PhotoInteraction.objects.filter(
+            user=request.user, 
+            photo=photo
+        ).first()
+    
+    context = {
+        'photo': photo,
+        'user_interaction': user_interaction,
+        'total_likes': photo.total_likes(),
+        'total_dislikes': photo.total_dislikes(),
+    }
+    return render(request, 'pic_me/photo_detail.html', context)
