047d17df1a9f004844c1c1ad80ca74771c866075
alvinp540 <alvinparsaloi@gmail.com>
2026-01-27 23:56:54 +0300

    admin.py update

--- pic_me/admin.py
+++ pic_me/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from .models import UserProfile, Photo, Tag, PhotoInteraction
 
-# Register your models here.
+User = get_user_model()
+
+
+@admin.register(User)
+class CustomUserAdmin(admin.ModelAdmin):
+	list_display = ('email', 'username', 'is_staff', 'is_active')
+	search_fields = ('email', 'username')
+
+
+admin.site.register(UserProfile)
+admin.site.register(Photo)
+admin.site.register(Tag)
+admin.site.register(PhotoInteraction)
